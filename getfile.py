8431efd02ebfb8a6dd7215edc5686f70d56bc7df
itchix <scot.scriven@gmail.com>
2015-04-23 22:07:01 +0200

    Gestion des dossiers
    Evite les sous dossiers

--- getfile.py
+++ getfile.py
@@ -1,18 +1,18 @@
 # -\*- coding: utf-8 -\*-
 
-import os
+import os, sys
 from urllib2 import urlopen, URLError, HTTPError
 from lxml import html
 import json
 import argparse
 
-def dlfile(url, name, verbose):
+def dlfile(url, path,  name, verbose):
     try:
         f = urlopen(url)
         if verbose != 0:
             print "downloading " + url
 
-        with open(os.path.basename(name), "wb") as local_file:
+        with open(os.path.join(path, name), "wb") as local_file:
             local_file.write(f.read())
 
     except HTTPError, e:
@@ -41,31 +41,55 @@ def main(w, t, v):
     tree = html.fromstring(data)
     id = tree.xpath("//div[@class='flip-entry']/@id")
     name = tree.xpath("//div[@class='flip-entry-title']/text()")
-    i = 0
+    title = tree.xpath("//div[@class='flip-folder-title']/span[@role=\"heading\"]/text()")
+    img = tree.xpath("//div[@class='flip-entry-thumb']/img/@src")
+    nomSousDossier = tree.xpath("//div[@class='flip-entry-icon']/div[@aria-label='Dossier']")
 
-    for oneid in id:
-        print '---------- FETCH URL -----------'
-        oneid = oneid[6:]
-        response2 = urlopen('https://drive.google.com/st/viewurls?id=' + oneid + '&m=1440')
-        data2 = response2.read()
-        data2 = data2[12:-2]
-        data2 = data2.replace("\u0026","&")
-        data2 = data2.replace("\u003d","=")
-        response3 = urlopen(data2)
-        data3 = response3.read()
-        data3 = data3[4:]
-        parsed_json = json.loads(data3)
-        dlfile(parsed_json[t], name[i], v)
-        i = i + 1
+    try:
+        if not os.path.exists(title[0]):
+           os.makedirs(title[0])
+    except OSError:
+        pass
+
+    nbSsDossier = len(nomSousDossier)
+    print nbSsDossier
+
+    i = 0
+    if t == "img":
+        for oneimg in img:
+            print '---------- FETCH IMG -----------'
+            oneimg = oneimg[:-5]
+            dlfile(oneimg,title[0],name[i],v)
+            i += 1
+
+    i = 0
+    if t == "pdf":
+        id = id[nbSsDossier:]
+        name = name[nbSsDossier:]
+        for oneid in id:
+            print '---------- FETCH PDF/TXT -----------'
+            oneid = oneid[6:]
+            response2 = urlopen('https://drive.google.com/st/viewurls?id=' + oneid + '&m=1440')
+            data2 = response2.read()
+            data2 = data2[12:-2]
+            data2 = data2.replace("\u0026","&")
+            data2 = data2.replace("\u003d","=")
+            response3 = urlopen(data2)
+            data3 = response3.read()
+            data3 = data3[4:]
+            parsed_json = json.loads(data3)
+            dlfile(parsed_json[t],title[0],name[i],v)
+            i += 1
+
+    print '------------------------------------'
+    print '-------------- FINISH --------------'
+    sys.exit
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('w', help="Put you drive folder link")
-    parser.add_argument('-v', '--verbose', help="Verbose mode (0,1) : Default 1", choices=[0,1,2], type=int, default="1", action="store")
-    parser.add_argument('-t', '--type', help="Put the type of files you want to fetch (default type : pdf)", default="pdf", action="store", choices=["pdf", "zip", "all", "txt", "doc", "png"])
+    parser.add_argument('-v', '--verbose', help="Verbose mode (0,1,2) : Default 1", choices=[0,1,2], type=int, default="1", action="store")
+    parser.add_argument('-t', '--type', help="Put the type of files you want to fetch (default type : doc/txt/pdf)", default="pdf", action="store", choices=["pdf", "zip", "all", "img"])
     args = parser.parse_args()
     main(args.w, args.type, args.verbose)
-
-
-
